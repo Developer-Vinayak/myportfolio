46b9e07d318594be10cf64a94166aa811266b10e
Developer-Vinayak <journey21210970@gmail.com>
2026-09-12 19:55:48 +0530

    Add blog pages as build entry points

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        blog: 'blog/blog.html',
+        post1: 'blog/posts/how-i-built-my-portfolio.html',
+        post2: 'blog/posts/how-i-learned-backend.html',
+      },
+    },
+  },
 });
